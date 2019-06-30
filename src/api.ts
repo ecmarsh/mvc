face0901889226c1a0b10493986e1dd563e2a8ca
Ethans-Macbook-Pro <ecmarsh@ucdavis.edu>
2019-06-30 16:42:01 -0700

    Add endpoint format mask

--- src/api.ts
+++ src/api.ts
@@ -9,27 +9,47 @@ interface ModelData {
 	id?: Id
 }
 
-const baseURL = `http://localhost:${process.env.DB_PORT}/`
+const baseURL = `http://localhost:${process.env.DB_PORT}`
 const api = axios.create({ baseURL })
-
 
 type POST = (endpoint: string, data: ModelData) => ApiPromise
 const createModel: POST = (endpoint, data) => {
-	return api.post(`/${endpoint}/`, data)
+	return api.post(`/${trimmed(endpoint)}/`, data)
 }
 
 type PUT = (endpoint: string, id: Id, data: ModelData) => ApiPromise
 const updateModel: PUT = (endpoint, id, data) => {
-	return api.put(`/${endpoint}/${id}`, data)
+	return api.put(`/${trimmed(endpoint)}/${id}`, data)
 }
 
 type GET = (endpoint: string, id: Id) => ApiPromise
 const getModel: GET = async (endpoint, id) => {
-	return api.get(`/${endpoint}/${id}`)
+	return api.get(`/${trimmed(endpoint)}/${id}`)
 }
+
 
 // API Types
 export { ApiResponse, ApiPromise, ApiError }
 
 // API Functions
 export { createModel, updateModel, getModel }
+
+// Helpers
+const trimSlashes = (str: string): string => {
+	const ref = [str]
+	const isLeadingSlash = /^\//.test(str)
+	const isTrailingSlash = /\/$/.test(str)
+	const trimLeadingSlash = () => { ref[0] = ref[0].substring(1) }
+	const trimTrailingSlash = () => { ref[0] = ref[0].substring(0, str.length - 1) }
+
+	if (isLeadingSlash) {
+		trimLeadingSlash()
+	}
+	if (isTrailingSlash) {
+		trimTrailingSlash()
+	}
+
+	const trimmedStr = ref[0]
+	return trimmedStr
+}
+const trimmed = trimSlashes

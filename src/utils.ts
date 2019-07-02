9f058f8f007cfa5e691883c8d25716487b5c0a69
Ethans-Macbook-Pro <ecmarsh@ucdavis.edu>
2019-07-02 15:23:52 -0700

    Refactor isUndefined

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,20 @@
-const trimSlashPadding = (str: string): string => {
+export const trimSlashPadding = (str: string): string => {
 	return str.replace(/^\/|\/$/g, '')
 }
 
 type AnyFn = (...args: any[]) => any
-const compareToStringEquality = (fn1: AnyFn, fn2: AnyFn) => {
+export const compareToStringEquality = (fn1: AnyFn, fn2: AnyFn) => {
 	return fn1.toString() === fn2.toString()
 }
 
-export { trimSlashPadding, compareToStringEquality }
+export const isUndefined = (arg: any): boolean => typeof arg === 'undefined'
+
+export const areUndefined = (...args: any[]): boolean => {
+	let flag = !args.length
+	args.forEach(arg => {
+		if (isUndefined(arg)) {
+			flag = true
+		}
+	})
+	return flag
+}

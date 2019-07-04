e13b26a6afffa027efb64e247e117385f0fb3614
Ethans-Macbook-Pro <ecmarsh@ucdavis.edu>
2019-07-03 20:59:48 -0700

    Type guard helper

--- src/utils.ts
+++ src/utils.ts
@@ -7,8 +7,8 @@ export const minify = (markup: string): string => {
 }
 
 type AnyFn = (...args: any[]) => any
-export const compareToStringEquality = (fn1: AnyFn, fn2: AnyFn) => {
-	return fn1.toString() === fn2.toString()
+export const compareToStringEquality = (fn1: AnyFn, fn2?: AnyFn) => {
+	return fn2 && fn1.toString() === fn2.toString()
 }
 
 type AnyObj = { [key: string]: any }
@@ -28,20 +28,55 @@ export const compareDeep = (o1: AnyObj, o2: AnyObj) => {
 		return true
 	}
 
-	const isBothPropsAndValsEqual = isPropsEq(o1, o2) && isPropsEq(o2, o1)
-	return !isBothPropsAndValsEqual
-}
-
-export const isUndefined = (arg: any): boolean => {
-	return typeof arg === 'undefined'
+	return isPropsEq(o1, o2) && isPropsEq(o2, o1)
 }
 
 export const areUndefined = (...args: any[]): boolean => {
 	let flag = !args.length
 	args.forEach(arg => {
-		if (isUndefined(arg)) {
+		if (is.Undefined(arg)) {
 			flag = true
 		}
 	})
 	return flag
+}
+
+type _Is = (value: any) => boolean
+interface IsTypes { [type: string]: _Is }
+export const is: IsTypes = (function () {
+	const _Undefined: _Is = x => typeof x === 'undefined'
+	const _Null: _Is = x => x === null
+	const _Nil: _Is = x => x == null
+	const _Number: _Is = x => typeof x == 'number'
+	const _String: _Is = x => typeof x == 'string'
+	const _Boolean: _Is = x => typeof x == 'boolean'
+	const _Symbol: _Is = x => typeof x == 'symbol'
+	const _Function: _Is = x => typeof x == 'function'
+	const _Object: _Is = x => x.toString() == '[object Object]'
+	const _Array: _Is = x => x.length && x.length >= 0
+
+	return {
+		Undefined: _Undefined,
+		Null: _Null,
+		Nil: _Nil,
+		Number: _Number,
+		String: _String,
+		Boolean: _Boolean,
+		Symbol: _Symbol,
+		Object: _Object,
+		Array: _Array,
+		Function: _Function,
+	}
+})()
+
+
+interface TypeGuard { [isGuardType: string]: Guard<GuardType> }
+type Guard<T> = (value: any) => value is T
+type GuardType = boolean | string | number | symbol
+
+export const typeGuard: TypeGuard = {
+	isBoolean: (value: any): value is boolean => is.Boolean(value),
+	isNumber: (value: any): value is number => is.Number(value),
+	isString: (value: any): value is string => is.String(value),
+	isSymbol: (value: any): value is symbol => is.Symbol(value),
 }
